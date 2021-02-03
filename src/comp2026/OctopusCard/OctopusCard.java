a3ad759d80b17ad79271f1b353ac7bfdc66e5787
Cyfine <58298819+Cyfine@users.noreply.github.com>
2021-02-02 23:02:30 -0800

    Update OctopusCard.java

--- src/comp2026/OctopusCard/OctopusCard.java
+++ src/comp2026/OctopusCard/OctopusCard.java
@@ -2,7 +2,7 @@
  * Copyright (C) 2020  CHENG Yifeng
 
    The source code is for reference only. Any other usage except reference, including but not
-   limited to distribute, copy will be considered as violation of academic integrity.
+   limited to distribute, copy for academic purposes will be considered as violation of academic integrity.
 
    You should make reasonable use of the source code of this program and abide
    by the rules of academic integrity. You should be held responsible for the punishment of plagiarism,
